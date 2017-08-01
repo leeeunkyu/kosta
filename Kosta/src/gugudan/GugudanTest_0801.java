827f781076d41b4a14b3cc0c856c775f4b0036bc
leeeunkyu <xdkyu01@gmail.com>
2017-08-01 19:26:08 +0900

    GugudanTest_0801

---
+++ Kosta/src/gugudan/GugudanTest_0801.java
@@ -0,0 +1,17 @@
+package gugudan;
+import java.io.IOException;
+
+public class GugudanTest_0801 {
+	public static void main(String[] args)  throws IOException 
+	{
+		System.out.println("1~9까지 숫자입력");
+		System.out.println("출력)");
+		int num=System.in.read()-48;
+		System.out.println("<"+num+"단>");
+		for(int i=1;i<10;i++)
+		{
+			System.out.println(num+" * "+i+" = " + num*i);
+		}
+
+	}
+}
